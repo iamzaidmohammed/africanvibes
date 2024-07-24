462b8b985e420167986961a5bd44bd8b7da5fb2f
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2024-07-24 15:35:12 +0000

    removed tailwind-scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import tailwindScrollbar from "tailwind-scrollbar";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -21,6 +19,5 @@ export default {
         "4xl": "2160px",
       },
     },
-    plugins: [tailwindScrollbar],
   },
 };
