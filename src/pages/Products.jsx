e332727b1f496c3649818c15f2288530f4845a11
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2024-07-28 14:26:38 +0000

    Fetched categories from db

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,11 +4,14 @@ import FlashSale from "../components/FlashSale";
 import Footer from "../components/Footer.jsx";
 import { Helmet } from "react-helmet";
 import { useEffect, useState } from "react";
+import Loading from "../components/Loading.jsx";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  // const [filteredProducts, setFilteredProducts] = useState([]);
+  // const [selectedCategory, setSelectedCategory] = useState("");
 
   useEffect(() => {
     fetch("/api/routes/products.php")
@@ -29,19 +32,24 @@ const Products = () => {
       });
   }, []);
 
+  // const handleCategorySelect = (category) => {
+  //   setSelectedCategory(category);
+  // };
+
+  // const handleCategorySelect = (category) => {
+  //   setSelectedCategory(category);
+  //   if (category) {
+  //     const filtered = products.filter(
+  //       (product) => product.categoryId === category.id
+  //     );
+  //     setFilteredProducts(filtered);
+  //   } else {
+  //     setFilteredProducts(products);
+  //   }
+  // };
+
   if (loading) {
-    return (
-      <div className="max-w-7xl md:mx-auto px-5 md:px-10 lg:px-20 flex items-center justify-center h-screen">
-        <div
-          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-          role="status"
-        >
-          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-            Loading...
-          </span>
-        </div>
-      </div>
-    );
+    return <Loading />;
   }
 
   if (error) {
@@ -61,28 +69,28 @@ const Products = () => {
         </div>
         <div className="flex flex-col md:flex-row">
           <FilterSection />
+
           <main className="p-4 w-full md:w-3/4">
             <h1 className="text-2xl font-bold mb-4">
-              Clothings and Textiles (250)
+              {/* {selectedCategory
+                ? `${selectedCategory.name} (${products.length})`
+                : `All Categories(${products.length})`} */}
+              category
             </h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
               {products.map((product) => (
                 <ProductsCard
                   key={product.id}
+                  name={product.name}
                   image={`/api/assets/images/${product.img}`}
-                  title={product.name}
                   price={product.price}
                 />
               ))}
             </div>
-            <div className="text-center mt-6">
-              <button className="px-4 py-2 bg-primary text-white rounded">
-                Browse More
-              </button>
-            </div>
           </main>
         </div>
         <FlashSale />
+
         <Footer />
       </section>
     </>
